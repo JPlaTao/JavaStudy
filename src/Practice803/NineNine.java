81e7b22883b1d10e6104d1748f7ac14829b233e6
PlaTao <jplatao@qq.com>
2023-08-06 16:04:40 +0800

    git test _806

--- src/Practice803/NineNine.java
+++ src/Practice803/NineNine.java
@@ -1,5 +1,5 @@
 
-public class NineNine {//git test
+public class NineNine {
     public static void main(String[] args) {
         for (int i = 1; i<= 9; i++){
             for(int j = 1; j <= i; j++){
