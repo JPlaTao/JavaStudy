89fb7d9d692f282623a83140e28ee897e73b9d9a
PlaTao <jplatao@qq.com>
2023-08-06 16:00:16 +0800

    git test _806

--- src/Practice803/For7sMultiple.java
+++ src/Practice803/For7sMultiple.java
@@ -1,5 +1,5 @@
 
-public class For7sMultiple {
+public class For7sMultiple {//git test
     public static void main(String[] args) {
         for (int i = 1; i * 7 <= 200; i++){
             System.out.print(i*7 + " ");
