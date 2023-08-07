8d2acace5f916e783ba1fe7fcb17f181a4f37aba
PlaTao <jplatao@qq.com>
2023-08-07 21:38:22 +0800

    6评委打分

---
+++ src/Practice805/Judge.java
@@ -0,0 +1,29 @@
+package Practice805;
+
+import java.util.Scanner;
+
+public class Judge {
+    public static void main(String[] args) {
+        int[] judge = new int[6];
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        for (int i = 0; i < judge.length; i++) {
+            System.out.println("第" + (i+1) + "位评委打分为：");
+            judge[i] = scanner.nextInt();
+            System.out.println(judge[i]);
+            sum += judge[i];
+        }
+        int max = 0;
+        for (int j : judge) {
+            if (j > max) max = j;
+        }
+        System.out.println(max);
+        int min = judge[0];
+        for (int j : judge) {
+            if (j<min) min = j;
+        }
+        System.out.println(min);
+        sum = sum - max - min;
+        System.out.println("最终得分为：" + sum/ (judge.length-2));
+    }
+}
