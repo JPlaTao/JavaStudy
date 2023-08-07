d4e21d4ae4a30f2fa4e6111a66114621dd37d811
PlaTao <jplatao@qq.com>
2023-08-07 21:40:22 +0800

    将一个随机数数组反转

---
+++ src/Practice805/Revers.java
@@ -0,0 +1,24 @@
+package Practice805;
+
+import java.util.Random;
+
+public class Revers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(20);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arr.length / 2; i++) {
+            arr[i] = arr[i] ^ arr[arr.length - 1 -i];
+            arr[arr.length - 1 - i] = arr[i] ^ arr[arr.length - 1 -i];
+            arr[i] = arr[i] ^ arr[arr.length - 1 - i];
+        }
+        for (int i: arr
+             ) {
+            System.out.print(i + " ");
+        }
+    }
+}
