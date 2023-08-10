dcef87e84c274c0b88b61e07515f4ed7d31071e5
PlaTao <jplatao@qq.com>
2023-08-10 22:11:26 +0800

    练习题2 莱布尼茨级数

---
+++ src/Practice810/Q2Leibniz.java
@@ -0,0 +1,27 @@
+package Practice810;
+
+import java.util.Scanner;
+
+public class Q2Leibniz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入莱布尼茨级数:");
+        int leibnizLevel = scanner.nextInt();
+        System.out.println(leibniz(leibnizLevel));
+    }
+
+    public static double leibniz(int level) {
+        double pi = 0;
+        double leveles = 0;
+        for (double i = 1; i <= level; i++) {
+            if (i % 2 == 1) {
+                leveles += 1 / (2 * i - 1);
+            } else {
+                leveles -= 1 / (2 * i - 1);
+            }
+        }
+        pi = 4 * leveles;
+        return pi;
+    }
+
+}
