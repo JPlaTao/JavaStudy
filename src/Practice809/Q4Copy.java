d3f0774e8bfbafa0f7999212a6121ffaac5d5312
PlaTao <jplatao@qq.com>
2023-08-10 22:09:08 +0800

    809 复制数组

--- src/Practice809/Q4Copy.java
+++ src/Practice809/Q4Copy.java
@@ -3,16 +3,15 @@ package Practice809;
 public class Q4Copy {
     public static void main(String[] args) {
         int[] nums = {2,11,33,44,55,66,277};
-
         int[] newNums = new int[nums.length-1];
 
 
-//        //(1)
-//        for (int i = 0,j= 0; j < nums.length; i++,j++) {
-//            if (nums[i] == 33){
-//                newNums[i] = nums[++j];
-//            }else newNums[i] = nums[j];
-//        }
+        //(1)
+        for (int i = 0,j= 0; j < nums.length; i++,j++) {
+            if (nums[i] == 33){
+                newNums[i] = nums[++j];
+            }else newNums[i] = nums[j];
+        }
 
 
         System.arraycopy(nums,0,newNums,0,2);
