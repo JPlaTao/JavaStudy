7a1a729bb8be4e7af6975e44fb6d1236e4d54c4c
PlaTao <jplatao@qq.com>
2023-08-13 22:51:57 +0800

    programing in exam_Q4 YangHuiTriangle (a better way)

--- src/Exam03/YangHuiTriangle.java
+++ src/Exam03/YangHuiTriangle.java
@@ -1,19 +1,29 @@
-//package Exam03;
-//
-//import java.util.Scanner;
-//
-//public class YangHuiTriangle {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int H = scanner.nextInt();
-//        int[][] arr = new int[H][H];
-//        for (int i = 0; i < H; i++) {
-//            arr[i][i] = 1;
-//        }
-//        for (int i = 2; i < H; i++) {
-//            for (int j = 0; j < ; j++) {
-//
-//            }
-//        }
-//    }
-//}
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class YangHuiTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入杨辉三角的高度：");
+        int H = scanner.nextInt();
+        int[][] yangHuiTri = new int[H][];
+        for (int i = 0; i < yangHuiTri.length; i++) {
+            yangHuiTri[i] = new int[i + 1];
+            yangHuiTri[i][0] = 1;
+            yangHuiTri[i][i] = 1;
+        }
+        fillTriangle(yangHuiTri);
+        for (int i = 0; i < yangHuiTri.length; i++) {
+            System.out.println(Arrays.toString(yangHuiTri[i]));
+        }
+
+    }
+    public static void fillTriangle(int[][] arr){
+        for (int i = 2; i < arr.length; i++) {
+            for (int j = 1; j < arr[i].length-1; j++) {
+                arr[i][j] = arr[i-1][j-1]+arr[i-1][j];
+            }
+        }
+    }
+}
