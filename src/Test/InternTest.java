c9599405e4f43cf84479ccff8264748dac799260
PlaTao <jplatao@qq.com>
2023-08-27 23:52:09 +0800

    some test

---
+++ src/Test/InternTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class InternTest {
+    public static void main(String[] args) {
+        String a ="a";
+        String s = new String("bc"+a);
+        String s1 = s.intern();
+        String str = "abc";
+        System.out.println(str == s);
+        System.out.println(str == s1);
+        System.out.println(s == s1);
+    }
+}
