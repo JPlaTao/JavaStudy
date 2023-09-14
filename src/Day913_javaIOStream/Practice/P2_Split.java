7e358a38627f69d0af9556b45d543f3acf2a8c66
PlaTao <jplatao@qq.com>
2023-09-14 23:11:48 +0800

    some change

--- src/Day913_javaIOStream/Practice/P2_Split.java
+++ src/Day913_javaIOStream/Practice/P2_Split.java
@@ -14,20 +14,21 @@ public class P2_Split {
         if (!tarFile.exists()) {
             tarFile.mkdirs();
         }
-        if (len>= Integer.MAX_VALUE){
-            e_lenBiggerThanIntMax(sourFile,tarFile,len);
-        }else
-            e_lenSmallerThanIntMax(sourFile,tarFile,(int)len);
+        if (len >= Integer.MAX_VALUE) {
+            e_lenBiggerThanIntMax(sourFile, tarFile, len);
+        } else
+            e_lenSmallerThanIntMax(sourFile, tarFile, (int) len);
     }
 
-    private static void e_lenSmallerThanIntMax(File sourFile, File tarFile, int len){
+    private static void e_lenSmallerThanIntMax(File sourFile, File tarFile, int len) {
         int size;
         int name = 0;
         byte[] byteSize = new byte[len];
         try (FileInputStream fis = new FileInputStream(sourFile)) {
             while ((size = fis.read(byteSize)) != -1) {
-                try (FileOutputStream fos = new FileOutputStream(tarFile.getPath() +
-                        File.separator + name++)) {
+                try (FileOutputStream fos =
+                             new FileOutputStream(tarFile.getPath() +
+                                     File.separator + name++)) {
                     fos.write(byteSize, 0, size);
                 }
             }
@@ -35,17 +36,23 @@ public class P2_Split {
             System.out.println(e.getMessage());
         }
     }
-    private static void e_lenBiggerThanIntMax(File sourFile, File tarFile, long len){
-        int size;
+
+    private static void e_lenBiggerThanIntMax(File sourFile, File tarFile, long len) {
+        int size = 0;
 //        int name = 0;
         int intLen = lessenLen(len);
         byte[] byteSize = new byte[intLen];
-        try(FileInputStream fis = new FileInputStream(sourFile)){
-            while ((size = fis.read(byteSize)) != -1){
-                try(FileOutputStream fos = new FileOutputStream(tarFile +
-                        File.separator + System.currentTimeMillis())){
-                    while (tarFile.length() < len){
-                        fos.write(byteSize,0,size);
+        try (FileInputStream fis = new FileInputStream(sourFile)) {
+            while (size != -1) {
+                File splitFile = new File(tarFile +
+                        File.separator + System.currentTimeMillis());
+                try (FileOutputStream fos =
+                             new FileOutputStream(splitFile, true)) {
+                    while (splitFile.length() < len) {
+                        size = fis.read(byteSize);
+                        if (size < 0)
+                            break;
+                        fos.write(byteSize, 0, size);
                     }
                 }
             }
@@ -54,10 +61,11 @@ public class P2_Split {
         }
     }
 
-    private static int lessenLen(long len){
-        if (len < Integer.MAX_VALUE-8)
-            return (int)len;
+    private static int lessenLen(long len) {
+        if (len < Integer.MAX_VALUE - 8)
+            return (int) len;
         return lessenLen(len / 2);
     }
+
 
 }
