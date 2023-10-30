16142e64d6b20e4196b13ca49643abfde2f84484
platao <jplatao@qq.com>
2023-10-30 21:46:43 +0800

    add SpringStudent

--- javaBase/Dayof923_JDBC/src/Example.java
+++ javaBase/Dayof923_JDBC/src/Example.java
@@ -2,16 +2,15 @@ import java.sql.*;
 
 public class Example {
     public static void main(String[] args) throws SQLException {
-        //TODO 1.注册数据库驱动
         DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
         //数据库连接字符串
         String url = "jdbc:mysql://localhost:3306/kfm";
-        try (//TODO 2.与数据库建立连接(构建连接对象)
+        try (
              Connection connector = DriverManager.getConnection(url, "root", "");
-             //TODO 4.构建执行 SQL的对象
+
              Statement statement = connector.createStatement();
         ) {
-            //TODO 3.写 SQL语句
+
 //            String insert = "insert into students values (77,'bb',33,'female','A')";
 //            String delete = ""
             //执行
